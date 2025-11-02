49d94d746770e7958dce1c2baa40d8fc4f31322f
Alan Shaw <alan@storacha.network>
2025-11-02 11:58:22 +0000

    refactor: move validation options to separate file

--- validator/validator.go
+++ validator/validator.go
@@ -81,49 +81,6 @@ type ValidationContext struct {
 	ValidateAuthorization ValidateAuthorizationFunc
 }
 
-type validationConfig struct {
-	canIssue              CanIssueFunc
-	parsePrincipal        PrincipalParserFunc
-	resolveProof          ProofResolverFunc
-	resolveDIDKey         DIDResolverFunc
-	validateAuthorization ValidateAuthorizationFunc
-}
-
-// Option is an option configuring the validator.
-type Option func(*validationConfig)
-
-// WithCanIssue informs validator whether given capability can be issued by a
-// given principal or whether it needs to be delegated to the issuer.
-func WithCanIssue(canIssue CanIssueFunc) Option {
-	return func(vc *validationConfig) {
-		vc.canIssue = canIssue
-	}
-}
-
-func WithPrincipalParser(parsePrincipal PrincipalParserFunc) Option {
-	return func(vc *validationConfig) {
-		vc.parsePrincipal = parsePrincipal
-	}
-}
-
-func WithProofResolver(resolveProof ProofResolverFunc) Option {
-	return func(vc *validationConfig) {
-		vc.resolveProof = resolveProof
-	}
-}
-
-func WithDIDResolver(resolveDIDKey DIDResolverFunc) Option {
-	return func(vc *validationConfig) {
-		vc.resolveDIDKey = resolveDIDKey
-	}
-}
-
-func WithAuthorizationValidator(validateAuthorization ValidateAuthorizationFunc) Option {
-	return func(vc *validationConfig) {
-		vc.validateAuthorization = validateAuthorization
-	}
-}
-
 // Access validates the invocation issuer is authorized to invoke the delegated
 // capability.
 //
