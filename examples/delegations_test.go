9d70d5705e9afabaf97472f169d83d8054093c9a
Alan Shaw <alan@storacha.network>
2025-11-10 20:15:42 +0000

    docs: delegations example

---
+++ examples/delegations_test.go
@@ -0,0 +1,58 @@
+package examples
+
+import (
+	"testing"
+
+	"github.com/alanshaw/ucantone/principal/ed25519"
+	"github.com/alanshaw/ucantone/ucan/command"
+	"github.com/alanshaw/ucantone/ucan/delegation"
+	"github.com/alanshaw/ucantone/ucan/delegation/policy"
+	"github.com/alanshaw/ucantone/ucan/delegation/policy/selector"
+)
+
+func TestDelegations(t *testing.T) {
+	// mailer is an email service that can send emails
+	mailer, err := ed25519.Generate()
+	if err != nil {
+		panic(err)
+	}
+
+	alice, err := ed25519.Generate()
+	if err != nil {
+		panic(err)
+	}
+
+	bob, err := ed25519.Generate()
+	if err != nil {
+		panic(err)
+	}
+
+	// delegate alice capability to use the email service
+	_, err = delegation.Delegate(
+		mailer,
+		alice,
+		must(command.Parse("/message/send")),
+		delegation.WithSubject(mailer),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// alice delegates bob capability to use the email service, but only allows
+	// bob to send to example.com email addresses
+	_, err = delegation.Delegate(
+		alice,
+		bob,
+		must(command.Parse("/message/send")),
+		delegation.WithSubject(mailer),
+		delegation.WithPolicy(
+			policy.All(
+				must(selector.Parse(".to")),
+				policy.Like(must(selector.Parse(".")), "*@example.com"),
+			),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
+}
