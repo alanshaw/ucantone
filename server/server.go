5aead2e8ad14ac09bf4578798d2c49d170f42f37
Alan Shaw <alan@storacha.network>
2025-12-02 11:27:17 +0000

    feat: wip

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io"
@@ -20,20 +21,20 @@ type HTTPExecRequest struct {
 	metadata   ucan.Container
 }
 
-func (h *HTTPExecRequest) Context() context.Context {
-	panic("unimplemented")
+func (r *HTTPExecRequest) Context() context.Context {
+	return r.Request.Context()
 }
 
-func (h *HTTPExecRequest) Invocation() ucan.Invocation {
-	return h.invocation
+func (r *HTTPExecRequest) Invocation() ucan.Invocation {
+	return r.invocation
 }
 
-func (h *HTTPExecRequest) Metadata() ucan.Container {
-	return h.metadata
+func (r *HTTPExecRequest) Metadata() ucan.Container {
+	return r.metadata
 }
 
-func (h *HTTPExecRequest) Task() ucan.Task {
-	return h.invocation.Task()
+func (r *HTTPExecRequest) Task() ucan.Task {
+	return r.invocation.Task()
 }
 
 var _ executor.ExecutionRequest = (*HTTPExecRequest)(nil)
@@ -43,16 +44,16 @@ type HTTPExecResponse struct {
 	metadata ucan.Container
 }
 
-func (h *HTTPExecResponse) SetMetadata(meta ucan.Container) error {
-	h.metadata = meta
+func (r *HTTPExecResponse) SetMetadata(meta ucan.Container) error {
+	r.metadata = meta
 	return nil
 }
 
-func (h *HTTPExecResponse) SetResult(o ipld.Any, x error) error {
+func (r *HTTPExecResponse) SetResult(o ipld.Any, x error) error {
 	if x != nil {
-		h.result = result.Error[ipld.Any, error](x)
+		r.result = result.Error[ipld.Any, error](x)
 	} else {
-		h.result = result.OK[ipld.Any, error](o)
+		r.result = result.OK[ipld.Any, error](o)
 	}
 	return nil
 }
@@ -72,8 +73,15 @@ func New(executor executor.Executor) *Server {
 
 // RoundTrip unpacks and executes an incoming request, returning the response.
 func (s *Server) RoundTrip(r *http.Request) (*http.Response, error) {
+	if r.Header.Get("Content-Type") != "application/vnd.ipld.dag-cbor" {
+		return nil, fmt.Errorf("invalid content type %s, expected application/vnd.ipld.dag-cbor", r.Header.Get("Content-Type"))
+	}
+
 	buf, err := io.ReadAll(r.Body)
-	ct, err := container.Decode(buf)
+	if err != nil {
+		return nil, fmt.Errorf("decoding container: %w", err)
+	}
+	reqContainer, err := container.Decode(buf)
 	if err != nil {
 		return nil, fmt.Errorf("decoding container: %w", err)
 	}
@@ -81,24 +89,54 @@ func (s *Server) RoundTrip(r *http.Request) (*http.Response, error) {
 	var receipts []ucan.Receipt
 	var invocations []ucan.Invocation
 	var delegations []ucan.Delegation
-	for _, inv := range ct.Invocations() {
-		req := HTTPExecRequest{r, inv, ct}
+	for _, inv := range reqContainer.Invocations() {
+		req := HTTPExecRequest{r, inv, reqContainer}
 		res := HTTPExecResponse{}
+
 		err := s.Executor.Execute(&req, &res)
 		if err != nil {
 			// This shouldn't really happen, executor only returns an error when
-			// result or  metadata cannot be set, which is likely a developer error.
+			// result or metadata cannot be set, which is likely a developer error.
 			return nil, fmt.Errorf("executing task %s: %w", inv.Task().Link(), err)
 		}
+
 		receipt, err := receipt.Issue(
 			s.ID,
 			inv.Link(),
-			out,
+			res.result,
 			receipt.WithCause(inv.Link()),
 		)
 		if err != nil {
 			return nil, fmt.Errorf("issuing receipt for task %s: %w", inv.Task().Link(), err)
 		}
 		receipts = append(receipts, receipt)
+
+		if res.metadata != nil {
+			invocations = append(invocations, res.metadata.Invocations()...)
+			delegations = append(delegations, res.metadata.Delegations()...)
+			receipts = append(receipts, res.metadata.Receipts()...)
+		}
 	}
+
+	respContainer, err := container.New(
+		container.WithInvocations(invocations...),
+		container.WithDelegations(delegations...),
+		container.WithReceipts(receipts...),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("creating response container: %w", err)
+	}
+
+	respBuf, err := container.Encode(container.Raw, respContainer)
+	if err != nil {
+		return nil, fmt.Errorf("encoding response container: %w", err)
+	}
+
+	resp := &http.Response{
+		StatusCode: http.StatusOK,
+		Body:       io.NopCloser(bytes.NewReader(respBuf)),
+		Header:     make(http.Header),
+	}
+	resp.Header.Set("Content-Type", "application/vnd.ipld.dag-cbor")
+	return resp, nil
 }
